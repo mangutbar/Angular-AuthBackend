7782110458ee581e6683c445882cee5cb74a00ea
Manuel Gutierrez <magutbar@gmail.com>
2023-03-30 13:57:07 +0200

    Primer commit backend

---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const router = Router();
+
+// Crear nuevo usuario
+router.post( '/new', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').isLength({ min: 6}),
+    validarCampos
+], crearUsuario );
+
+// Login de usuario
+router.post( '/', [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').isLength({ min: 6}),
+    validarCampos
+], loginUsuario );
+
+// Login de usuario
+router.get( '/renew', revalidarToken );
+
+
+module.exports = router;
