fa17cf4e82461bc7fe8312abb0b850dfbefb8363
Manuel Gutierrez <magutbar@gmail.com>
2023-04-03 10:25:41 +0200

    Actualizacion backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,6 +36,7 @@ const crearUsuario = async(req, res = response) => {
             ok: true,
             uid: dbUser.id,
             name,
+            email,
             token
         });
         
@@ -84,6 +85,7 @@ const loginUsuario = async(req, res = response) => {
             ok: true,
             uid: dbUser.id,
             name: dbUser.name,
+            email: dbUser.email,
             token
         });
 
@@ -103,15 +105,19 @@ const loginUsuario = async(req, res = response) => {
 
 const revalidarToken = async(req, res = response) => {
 
-    const { uid, name } = req;
+    const { uid } = req;
+
+    // Leer base de datos
+    const dbUser = await Usuario.findById(uid);
 
     // Generar el JWT
-    const token = await generarJWT( uid, name );
+    const token = await generarJWT( uid, dbUser.name );
 
     return res.json({
         ok: true,
         uid,
-        name,
+        name: dbUser.name,
+        email: dbUser.email,
         token
     });
 
